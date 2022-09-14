439a5cb8f82f1b293a7b2a6cef35ad5ed8758c00
miam500 <78931755+miam500@users.noreply.github.com>
2022-09-14 11:19:53 -0400

    Update fuzzyLogic.py

--- APP1/code_depart_IA_APP1/code_depart/fuzzyLogic.py
+++ APP1/code_depart_IA_APP1/code_depart/fuzzyLogic.py
@@ -15,10 +15,10 @@ def createFuzzyController():
 
     #in
     direction = ctrl.Antecedent(np.linspace(-1, 1, 1000), 'direction')
-    v_item = ctrl.Antecedent(np.linspace(-1, 1, 1000), 'v_item')
-    a_item = ctrl.Antecedent(np.linspace(-1, 1, 1000), 'a_item')
-    v_ob = ctrl.Antecedent(np.linspace(-30, 30, 1000), 'v_ob')
-    a_ob = ctrl.Antecedent(np.linspace(-1, 1, 1000), 'a_ob')
+    up_p = ctrl.Antecedent(np.linspace(0, 100, 1000), 'up_p')
+    down_p = ctrl.Antecedent(np.linspace(0, 100, 1000), 'down_p')
+    left_p = ctrl.Antecedent(np.linspace(0, 100, 1000), 'left_p')
+    right_p = ctrl.Antecedent(np.linspace(0, 100, 1000), 'right_p')
 
 
     #out
@@ -39,8 +39,17 @@ def createFuzzyController():
     direction['left'] = fuzz.trimf(direction.universe, [0, 0.25, 0.5])
     direction['right'] = fuzz.trimf(direction.universe, [0.5, 0.75, 1])
 
-    x_ob['left'] = fuzz.trapmf(x_ob.universe, [-30, -30, -10, 0])
-    x_ob['right'] = fuzz.trapmf(x_ob.universe, [0, 10, 30, 30])
+    up_p['leave'] = fuzz.trimf(up_p.universe, [0, 25, 50])
+    up_p['go_to'] = fuzz.trimf(up_p.universe, [50, 75, 100])
+
+    down_p['leave'] = fuzz.trimf(down_p.universe, [-10, -10, 0])
+    down_p['go_to'] = fuzz.trimf(down_p.universe, [0, 10, 10])
+
+    right_p['leave'] = fuzz.trimf(right_p.universe, [0, 25, 50])
+    right_p['go_to'] = fuzz.trimf(right_p.universe, [25, 75, 100])
+
+    left_p['leave'] = fuzz.trimf(left_p.universe, [0, 25, 50])
+    left_p['go_to'] = fuzz.trimf(left_p.universe, [50, 75, 100])
 
     move_x['left'] = fuzz.trapmf(move_x.universe, [-1, -1, 0, 0])
     move_x['right'] = fuzz.trapmf(move_x.universe, [0, 0, 1, 1])
@@ -49,36 +58,93 @@ def createFuzzyController():
 
     # TODO: Define the rules.
     rules = []
-    #-----------------------------------------------------------------------------------------------#
+    #------------------------------------------------------------------------------------------------------------------#
+    #-----------------------------------------------UP-----------------------------------------------------------------#
+    #------------------------------------------------------------------------------------------------------------------#
     rules.append(ctrl.Rule(antecedent=(direction['up']),
-                           consequent=(move_x['left'], move_y['up'])))
+                           consequent=(move_y['up'])))
+    rules.append(ctrl.Rule(antecedent=(direction['up'] & up_p['go_to'] & left_p['go_to']),
+                           consequent=(move_y['up'], move_x['left'])))
+    rules.append(ctrl.Rule(antecedent=(direction['up'] & up_p['go_to'] & left_p['leave']),
+                           consequent=(move_y['up'], move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['up'] & up_p['go_to'] & right_p['go_to']),
+                           consequent=(move_y['up'], move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['up'] & up_p['go_to'] & right_p['leave']),
+                           consequent=(move_y['up'], move_x['left'])))
+    rules.append(ctrl.Rule(antecedent=(direction['up'] & up_p['leave'] & left_p['go_to']),
+                           consequent=(move_x['left'])))
+    rules.append(ctrl.Rule(antecedent=(direction['up'] & up_p['leave'] & left_p['leave']),
+                           consequent=(move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['up'] & up_p['leave'] & right_p['go_to']),
+                           consequent=(move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['up'] & up_p['leave'] & right_p['leave']),
+                           consequent=(move_x['left'])))
+    # -----------------------------------------------------------------------------------------------------------------#
+    # -----------------------------------------------DOWN--------------------------------------------------------------#
+    # -----------------------------------------------------------------------------------------------------------------#
     rules.append(ctrl.Rule(antecedent=(direction['down']),
-                           consequent=(move_x['left'], move_y['down'])))
+                           consequent=(move_y['down'])))
+    rules.append(ctrl.Rule(antecedent=(direction['down'] & down_p['go_to'] & left_p['go_to']),
+                           consequent=(move_y['down'], move_x['left'])))
+    rules.append(ctrl.Rule(antecedent=(direction['down'] & down_p['go_to'] & left_p['leave']),
+                           consequent=(move_y['down'], move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['down'] & down_p['go_to'] & right_p['go_to']),
+                           consequent=(move_y['dwon'], move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['down'] & down_p['go_to'] & right_p['leave']),
+                           consequent=(move_y['down'], move_x['left'])))
+    rules.append(ctrl.Rule(antecedent=(direction['down'] & down_p['leave'] & left_p['go_to']),
+                           consequent=(move_x['left'])))
+    rules.append(ctrl.Rule(antecedent=(direction['down'] & down_p['leave'] & left_p['leave']),
+                           consequent=(move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['down'] & down_p['leave'] & right_p['go_to']),
+                           consequent=(move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['down'] & down_p['leave'] & right_p['leave']),
+                           consequent=(move_x['left'])))
+    # -----------------------------------------------------------------------------------------------------------------#
+    # -----------------------------------------------LEFT--------------------------------------------------------------#
+    # -----------------------------------------------------------------------------------------------------------------#
     rules.append(ctrl.Rule(antecedent=(direction['left']),
+                           consequent=(move_x['left'])))
+    rules.append(ctrl.Rule(antecedent=(direction['left'] & left_p['go_to'] & down_p['go_to']),
                            consequent=(move_x['left'], move_y['down'])))
-    rules.append(ctrl.Rule(antecedent=(direction['right']),
-                           consequent=(move_x['right'], move_y['up'])))
-    # -----------------------------------------------------------------------------------------------#
-    rules.append(ctrl.Rule(antecedent=(direction['up'] & x_ob['left']),
-                           consequent=(move_x['right'], move_y['up'])))
-    rules.append(ctrl.Rule(antecedent=(direction['down'] & x_ob['left']),
-                           consequent=(move_x['right'], move_y['down'])))
-    rules.append(ctrl.Rule(antecedent=(direction['left'] & x_ob['left']),
-                           consequent=(move_x['left'], move_y['down'])))
-    rules.append(ctrl.Rule(antecedent=(direction['right'] & x_ob['left']),
-                           consequent=(move_x['right'], move_y['up'])))
-
-    rules.append(ctrl.Rule(antecedent=(direction['up'] & x_ob['right']),
+    rules.append(ctrl.Rule(antecedent=(direction['left'] & left_p['go_to'] & down_p['leave']),
                            consequent=(move_x['left'], move_y['up'])))
-    rules.append(ctrl.Rule(antecedent=(direction['down'] & x_ob['right']),
+    rules.append(ctrl.Rule(antecedent=(direction['left'] & left_p['go_to'] & up_p['go_to']),
+                           consequent=(move_x['left'], move_y['up'])))
+    rules.append(ctrl.Rule(antecedent=(direction['left'] & left_p['go_to'] & up_p['leave']),
                            consequent=(move_x['left'], move_y['down'])))
-    rules.append(ctrl.Rule(antecedent=(direction['left'] & x_ob['right']),
-                           consequent=(move_x['left'], move_y['down'])))
-    rules.append(ctrl.Rule(antecedent=(direction['right'] & x_ob['right']),
-                           consequent=(move_x['right'], move_y['up'])))
-
+    rules.append(ctrl.Rule(antecedent=(direction['left'] & left_p['leave'] & down_p['go_to']),
+                           consequent=(move_y['down'])))
+    rules.append(ctrl.Rule(antecedent=(direction['left'] & left_p['leave'] & down_p['leave']),
+                           consequent=(move_y['up'])))
+    rules.append(ctrl.Rule(antecedent=(direction['left'] & left_p['leave'] & up_p['go_to']),
+                           consequent=(move_y['up'])))
+    rules.append(ctrl.Rule(antecedent=(direction['left'] & left_p['leave'] & up_p['leave']),
+                           consequent=(move_y['down'])))
+    # -----------------------------------------------------------------------------------------------------------------#
+    # -----------------------------------------------RIGHT-------------------------------------------------------------#
+    # -----------------------------------------------------------------------------------------------------------------#
+    rules.append(ctrl.Rule(antecedent=(direction['right']),
+                           consequent=(move_x['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['right'] & right_p['go_to'] & down_p['go_to']),
+                           consequent=(move_x['right'], move_y['down'])))
+    rules.append(ctrl.Rule(antecedent=(direction['right'] & right_p['go_to'] & down_p['leave']),
+                           consequent=(move_x['right'], move_y['right'])))
+    rules.append(ctrl.Rule(antecedent=(direction['right'] & right_p['go_to'] & up_p['go_to']),
+                           consequent=(move_x['up'], move_y['up'])))
+    rules.append(ctrl.Rule(antecedent=(direction['right'] & right_p['go_to'] & up_p['leave']),
+                           consequent=(move_x['up'], move_y['down'])))
+    rules.append(ctrl.Rule(antecedent=(direction['right'] & right_p['leave'] & down_p['go_to']),
+                           consequent=(move_y['down'])))
+    rules.append(ctrl.Rule(antecedent=(direction['right'] & right_p['leave'] & down_p['leave']),
+                           consequent=(move_y['up'])))
+    rules.append(ctrl.Rule(antecedent=(direction['right'] & right_p['leave'] & up_p['go_to']),
+                           consequent=(move_y['up'])))
+    rules.append(ctrl.Rule(antecedent=(direction['right'] & right_p['leave'] & up_p['leave']),
+                           consequent=(move_y['down'])))
 
     for rule in rules:
+        #somme ou somme ponderer possible
         rule.and_func = np.fmin
         rule.or_func = np.fmax
 
@@ -87,4 +153,3 @@ def createFuzzyController():
     system = ctrl.ControlSystem(rules)
     sim = ctrl.ControlSystemSimulation(system)
     return sim
-
